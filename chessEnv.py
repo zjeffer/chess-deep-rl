17b797391c536886f42ea66107fb79db293010d3
zjeffer <4633209+zjeffer@users.noreply.github.com>
2021-12-28 15:05:09 +0100

    spaces to tabs

--- chessEnv.py
+++ chessEnv.py
@@ -2,83 +2,85 @@ import chess
 import numpy as np
 
 # class for chess environment
+
+
 class ChessEnv:
-    def __init__(self):
-        """
-        Initialize the chess environment
-        """
-        # the chessboard
-        self.board = chess.Board()
+	def __init__(self):
+		"""
+		Initialize the chess environment
+		"""
+		# the chessboard
+		self.board = chess.Board()
 
-    def reset(self):
-        """
-        Reset everything
-        """
-        self.board = chess.Board()
+	def reset(self):
+		"""
+		Reset everything
+		"""
+		self.board = chess.Board()
 
-    @staticmethod
-    def board_to_booleans(board: chess.Board):
-        """
-        Convert the board to a boolean representation
-        """
-        booleans = []
-        # for every player
-        for color in chess.COLORS:
-            # create an array of boards
-            booleans.append([])
-            for piece_type in chess.PIECE_TYPES:
-                indexes = list(board.pieces(piece_type, color))
-                # create the array for the current piece type
-                array = [[0 for x in range(8)] for y in range(8)]
-                # for every index, set the array to true
-                for index in indexes:
-                    array[int(index/8)][index % 8] = 1
-                booleans[-1].append(array)
-        return booleans
+	@staticmethod
+	def board_to_booleans(board: chess.Board):
+		"""
+		Convert the board to a boolean representation
+		"""
+		booleans = []
+		# for every player
+		for color in chess.COLORS:
+			# create an array of boards
+			booleans.append([])
+			for piece_type in chess.PIECE_TYPES:
+				indexes = list(board.pieces(piece_type, color))
+				# create the array for the current piece type
+				array = [[0 for x in range(8)] for y in range(8)]
+				# for every index, set the array to true
+				for index in indexes:
+					array[int(index/8)][index % 8] = 1
+				booleans[-1].append(array)
+		return booleans
 
-    @staticmethod
-    def board_to_state(board: chess.Board):
-        """
-        Convert board to a state that is interpretable by the model
-        """
-        if len(board.move_stack) == 0:
-            return ChessEnv.board_to_booleans(board)
+	@staticmethod
+	def board_to_state(board: chess.Board):
+		"""
+		Convert board to a state that is interpretable by the model
+		"""
+		if len(board.move_stack) == 0:
+			return ChessEnv.board_to_booleans(board)
 
-        # put history of chess board into a list
-        boards = []
-        curr_board = board.copy()
-        # put last 10 moves in state
-        history_amount = 10
-        # if there are less than 10 moves, put all of them in state
-        if len(curr_board.move_stack) < history_amount:
-            history_amount = len(curr_board.move_stack)
-        for move in board.move_stack[-history_amount:][::-1]:
-            curr_board.undo(move)
-            booleans = ChessEnv.board_to_booleans(curr_board)
-            boards.append(booleans)
-        # TODO: Right now, the boards[] array contains the last 10 boards, 
-        # but do i need to separate the colors like AlphaGo Zero does?
+		# put history of chess board into a list
+		boards = []
+		curr_board = board.copy()
+		# put last 10 moves in state
+		history_amount = 10
+		# if there are less than 10 moves, put all of them in state
+		if len(curr_board.move_stack) < history_amount:
+			history_amount = len(curr_board.move_stack)
+		for move in board.move_stack[-history_amount:][::-1]:
+			curr_board.undo(move)
+			booleans = ChessEnv.board_to_booleans(curr_board)
+			boards.append(booleans)
+		# TODO: Right now, the boards[] array contains the last 10 boards,
+		# but do i need to separate the colors like AlphaGo Zero does?
 
-        return boards
+		return boards
 
-    @staticmethod
-    def print_board(board: list):
-        """
-        Print the board
-        """
-        for row in board:
-            for col in row:
-                print(col)
+	@staticmethod
+	def print_board(board: list):
+		"""
+		Print the board
+		"""
+		for row in board:
+			for col in row:
+				print(col)
 
-    def move(self, action: chess.Move):
-        """ 
-        Perform an action on the board
-        """
-        pass
+	def move(self, action: chess.Move):
+		""" 
+		Perform an action on the board
+		"""
+		pass
 
 
 if __name__ == "__main__":
-    chessEnv = ChessEnv()
-    state = chessEnv.board_to_state(chessEnv.board)
-    ChessEnv.print_board(state[0])
-    print(np.array(state).shape)
+	chessEnv = ChessEnv()
+	state = chessEnv.board_to_state(chessEnv.board)
+	ChessEnv.print_board(state[0])
+	print(np.array(state).shape)
--- mcts.py
+++ mcts.py
@@ -4,42 +4,42 @@ import chess
 import random
 
 class MCTS:
-    def __init__(self):
-        self.root = Node(state=chess.Board())
-        self.amount_of_simulations = 0
-        
-    def run_simulation(self):
-        node = self.root
-        # select a leaf node to expand
-        leaf = self.select_child(node)
-        # expand the leaf node
-        actions = leaf.get_unexplored_actions()
-        
-        # rollout the leaf node
+	def __init__(self):
+		self.root = Node(state=chess.Board())
+		self.amount_of_simulations = 0
+		
+	def run_simulation(self):
+		node = self.root
+		# select a leaf node to expand
+		leaf = self.select_child(node)
+		# expand the leaf node
+		actions = leaf.get_unexplored_actions()
+		
+		# rollout the leaf node
 
-        # backpropagate the result
+		# backpropagate the result
 
-    def select_child(self, node):
-        # find a leaf node
-        while not node.is_leaf():
-            node = random.choice(node.children)
-        return node
+	def select_child(self, node):
+		# find a leaf node
+		while not node.is_leaf():
+			node = random.choice(node.children)
+		return node
 
 
-    def expand(self):
-        # add a new child to the leaf
-        leaf = self.select_child(self.root)
-        # take a random action
-        action = random.choice(leaf.get_unexplored_actions())
-        leaf.step(action)
-        # get the new state
-        new_state = leaf.state.copy()
-        # create a new node with the new state
-        leaf.add_child(Node(state=new_state, parent=leaf, action=action))
-        
+	def expand(self):
+		# add a new child to the leaf
+		leaf = self.select_child(self.root)
+		# take a random action
+		action = random.choice(leaf.get_unexplored_actions())
+		leaf.step(action)
+		# get the new state
+		new_state = leaf.state.copy()
+		# create a new node with the new state
+		leaf.add_child(Node(state=new_state, parent=leaf, action=action))
+		
 
-    def rollout(self):
-        pass
+	def rollout(self):
+		pass
 
-    def backpropagate(self):
-        pass
+	def backpropagate(self):
+		pass
