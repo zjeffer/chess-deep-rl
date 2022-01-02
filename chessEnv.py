389950a9363215bf8422f2414062475f58a4eda2
zjeffer <4633209+zjeffer@users.noreply.github.com>
2022-01-02 15:41:06 +0100

    removed unused board_to_booleans method

--- chessEnv.py
+++ chessEnv.py
@@ -26,26 +26,6 @@ class ChessEnv:
         self.board = chess.Board()
 
     @staticmethod
-    def board_to_booleans(board: chess.Board):
-        """
-        Convert the board to a boolean representation
-        """
-        booleans = []
-        # for every player
-        for color in chess.COLORS:
-            # create an array of boards
-            booleans.append([])
-            for piece_type in chess.PIECE_TYPES:
-                indexes = list(board.pieces(piece_type, color))
-                # create the array for the current piece type
-                array = [[0 for x in range(8)] for y in range(8)]
-                # for every index, set the array to true
-                for index in indexes:
-                    array[int(index/8)][index % 8] = 1
-                booleans[-1].append(array)
-        return booleans
-
-    @staticmethod
     def board_to_state(board: chess.Board) -> np.ndarray((8, 8, 20)):
         """
         Convert board to a state that is interpretable by the model
@@ -127,10 +107,3 @@ class ChessEnv:
         Perform an action on the board
         """
         pass
-
-
-if __name__ == "__main__":
-    chessEnv = ChessEnv()
-    state = chessEnv.board_to_state(chessEnv.board)
-    ChessEnv.print_board(state[0])
-    print(np.array(state).shape)
