f580126a464026e361bb219bb9b591f4077e8ac6
zjeffer <4633209+zjeffer@users.noreply.github.com>
2022-01-02 15:21:02 +0100

    test & utils class: test input state works, i can now save the full input state to an image

---
+++ test.py
@@ -0,0 +1,54 @@
+from agent import Agent
+from chessEnv import ChessEnv
+import utils
+import logging
+logging.basicConfig(level=logging.INFO)
+import time
+
+def timer_function(func):
+	def wrap_func(*args, **kwargs):
+		t1 = time.time()
+		result = func(*args, **kwargs)
+		t2 = time.time()
+		print(f'Function {func.__name__!r} executed in {(t2-t1):.4f}s')
+		return result
+	return wrap_func
+
+class Test:
+	def __init__(self):
+		self.reset()
+	
+	def reset(self):
+		# don't build the model (it's not needed for these tests)
+		white = Agent(build_model=False)
+		black = Agent(build_model=False)
+		self.env = ChessEnv(white, black)
+
+	@timer_function
+	def run_tests(self, n: int=1):
+
+		for _ in range(n):
+
+			self.reset()
+			# test en passant
+			self.env.board.push_san("e4")
+			self.env.board.push_san("a5")
+			self.env.board.push_san("e5")
+			self.env.board.push_san("Nc6")
+			self.env.board.push_san("d4")
+			self.env.board.push_san("d5")
+			
+
+			# test input_state
+			input_state = self.env.board_to_state(self.env.board)
+
+		# names = ['white_turn', 'black_turn', 
+		# 	'castling_king_white', 'castling_king_black', 'castling_queen_white', 'castling_queen_black', 
+		# 	'is_repitition', 
+		# 	'white_pawns', 'white_knights', 'white_bishops', 'white_rooks', 'white_queens', 'white_king', 
+		# 	'black_pawns', 'black_knights', 'black_bishops', 'black_rooks', 'black_queens', 'black_king',
+		# 	'en_passant']
+		# utils.save_input_state_to_imgs(input_state, 'tests/input_planes', names)
+
+ 
+	
---
+++ utils.py
@@ -0,0 +1,33 @@
+import numpy as np
+from PIL import Image
+import time
+
+
+def save_input_state_to_imgs(input_state: np.ndarray, path: str, names: list = None, only_full: bool = False):
+	"""
+	Save the input state to images
+	"""
+	start_time = time.time()
+	if not only_full:
+		for index, plane in enumerate(input_state):
+			# save boolean 2d array to image
+			img = Image.fromarray(plane)
+			# save image
+			if names is not None and len(names) == len(input_state):
+				# print index, with one leading 0
+				img.save(f"{path}/{index:02d}-{names[index]}.png")
+			else:
+				img.save(f"{path}/{index:02d}.png")
+
+	# full image of all states
+	# convert booleans to integers
+	input_state = np.array(input_state)*np.uint8(255)
+	# pad input_state with grey values
+	input_state = np.pad(input_state, ((0, 0), (1, 1), (1, 1)), 'constant', constant_values=128)
+	
+	full_array = np.concatenate(input_state, axis=1)
+	# pad 1 column left and right of image
+	full_array = np.pad(full_array, ((4, 4), (5, 5)), 'constant', constant_values=128)
+	img = Image.fromarray(full_array)
+	img.save(f"{path}/full.png")
+	print(f"*** Saving to images: {(time.time() - start_time):.6f} seconds ***")
