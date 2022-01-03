1b49c98cda56864af12f57b48fb04783b0622032
zjeffer <4633209+zjeffer@users.noreply.github.com>
2022-01-03 14:06:58 +0100

    output vector to image method

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import numpy as np
 from PIL import Image
 import time
 
+from numpy.core.numeric import full
+
 
 def save_input_state_to_imgs(input_state: np.ndarray, path: str, names: list = None, only_full: bool = False):
     """
@@ -27,10 +29,30 @@ def save_input_state_to_imgs(input_state: np.ndarray, path: str, names: list = N
                          'constant', constant_values=128)
 
     full_array = np.concatenate(input_state, axis=1)
-    # pad 1 column left and right of image
+    # more padding
     full_array = np.pad(full_array, ((4, 4), (5, 5)),
                         'constant', constant_values=128)
     img = Image.fromarray(full_array)
+    img.save(f"{path}/full.png")
+    print(
+        f"*** Saving to images: {(time.time() - start_time):.6f} seconds ***")
+
+
+def save_output_state_to_imgs(output_state: np.ndarray, path: str):
+    """
+    Save the output state to images
+    """
+    start_time = time.time()
+    # full image of all states
+    # convert booleans to integers
+    # pad input_state with grey values
+    output_state = np.pad(output_state.astype(int)*255, ((0, 0), (1, 1), (1, 1)), 'constant', constant_values=128)
+    full_array = np.concatenate(output_state, axis=1)
+    # more padding
+    full_array = np.pad(full_array, ((4, 4), (5, 5)), 'constant', constant_values=128)
+    img = Image.fromarray(full_array.astype(np.uint8))
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
     img.save(f"{path}/full.png")
     print(
         f"*** Saving to images: {(time.time() - start_time):.6f} seconds ***")
