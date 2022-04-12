7525ca04ee1f0154c82b96a639f26c71798f46c0
zjeffer <4633209+zjeffer@users.noreply.github.com>
2022-04-12 21:47:54 +0200

    added script for building & pushing to ghcr.io

---
+++ code/scripts/docker-build-and-push.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+echo -e "========== Building server image\n"
+docker build -f Dockerfile.server -t chess-rl_prediction-server .
+
+if [ $? -ne 0 ]; then
+	echo "Failed to build server image"
+	exit 1
+fi
+
+echo -e "\n========== Server image built"
+echo -e "==========Building client image\n"
+docker build -f Dockerfile.client -t chess-rl_selfplay-client .
+
+if [ $? -ne 0 ]; then
+	echo "Failed to build client image"
+	exit 1
+fi
+
+echo -e "\n========== Client image built"
+echo -e "========== Pushing server image\n"
+docker push ghcr.io/zjeffer/chess-rl_prediction-server
+
+if [ $? -ne 0 ]; then
+	echo "Failed to push server image"
+	exit 1
+fi
+
+echo -e "\n========== Pushed server image"
+echo -e "========== Pushing client image\n"
+docker push ghcr.io/zjeffer/chess-rl_selfplay-client
+
+if [ $? -ne 0 ]; then
+	echo "Failed to push client image"
+	exit 1
+fi
+
+echo -e "========== Pushed client image"
+echo -e "\n========== Images built & pushed succesfully. Exiting..."
